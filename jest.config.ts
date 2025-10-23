c4846ce220599b1b8ba5b8bf248c7c3ecc6927ce
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-23 17:58:35 +0300

    Added jest configuration and package

---
+++ jest.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    // setupFiles: ['<rootDir>/tests/setup/env.ts'],
+    // setupFilesAfterEnv: ["<rootDir>/tests/setup/setup.ts"],
+    // globalSetup: '<rootDir>/tests/setup/globalSetup.ts',
+
+    testMatch: [
+        '<rootDir>/src/**/*.spec.ts',
+        '<rootDir>/src/**/*.test.ts',
+        '<rootDir>/tests/**/*.(spec|test).ts'
+    ],
+    // collectCoverage: true,
+    // collectCoverageFrom: [
+    //     'src/**/*.{ts,tsx}',
+    //     '!src/**/*.spec.ts', 
+    //     '!src/**/*.test.ts',
+    //     '!src/types/**',     
+    //     '!src/config/**',  
+    //     '!src/app.ts',      
+    // ],
+    // coverageDirectory: 'coverage', 
+    // coverageReporters: ['text', 'lcov', 'html'], 
+    // coverageThreshold: {
+    //     'global': {
+    //         'statements': 85,
+    //         'branches': 85,
+    //         'functions': 85,
+    //         'lines': 85
+    //     }
+    // },
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
+};
+
+export default config;
