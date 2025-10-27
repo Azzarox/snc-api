cdcda1db357c15bdbfd16480fe20fcc95b872b70
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-27 18:42:55 +0200

    Added success response to register user

--- src/controllers/auth/authController.ts
+++ src/controllers/auth/authController.ts
@@ -1,6 +1,8 @@
 import { Context } from "koa";
 import { authService } from "../../services/auth/authService";
 import { RegisterPayload } from "../../schemas/auth/registerSchema";
+import { SuccessResponse } from "../../common/response/Response";
+import { StatusCodes } from "http-status-codes";
 
 export const users: { username: string, password: string }[] = [];
 
@@ -8,8 +10,10 @@ export const users: { username: string, password: string }[] = [];
 const registerUser = (ctx: Context) => {
     const { username, password } = ctx.request.body as RegisterPayload;
     const user = authService.registerUser(username, password);
-    ctx.status = 201;
-    ctx.body = user;
+
+    const response = new SuccessResponse(StatusCodes.CREATED, 'Successfully created user!', user)
+    ctx.status = response.status
+    ctx.body = response
 }
 
 export const authController = {
