1c7440d0dde481de650dddbf3016bdc37b88bbcf
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-11-13 11:01:30 +0200

    Fixed seeds to have avatar url

--- seeds/20251102202825_user_profiles.ts
+++ seeds/20251102202825_user_profiles.ts
@@ -1,6 +1,7 @@
 import { Knex } from 'knex';
 import { DB_USER_SEEDS } from './20251102202749_users';
 import { UserProfileEntity } from '../src/schemas/entities/userProfileEntitySchema';
+import { generateDefaultAvatarUrl } from '../src/utils/generateAvatarImage';
 
 const DB_USER_PROFILE_SEEDS: Record<string, Partial<UserProfileEntity>> = {
 	test123: {
@@ -8,6 +9,7 @@ const DB_USER_PROFILE_SEEDS: Record<string, Partial<UserProfileEntity>> = {
 		lastName: 'Testing',
 		bio: 'This is user Test Testing bio',
 		description: 'I am user test testing ...',
+		avatarStorageKey: null,
 	},
 	sarah: {
 		firstName: 'Sarah',
@@ -19,6 +21,7 @@ const DB_USER_PROFILE_SEEDS: Record<string, Partial<UserProfileEntity>> = {
 		contemporary songwriting. I love sharing my
 		passion for music and helping others on
 		their guitar journey.`,
+		avatarStorageKey: null,
 	},
 };
 
@@ -42,6 +45,7 @@ export async function seed(knex: Knex): Promise<void> {
 		}
 
 		userProfile.userId = user.id;
+		userProfile.avatarUrl = generateDefaultAvatarUrl(user.id);
 
 		await knex('user_profiles').insert([userProfile]);
 	}
