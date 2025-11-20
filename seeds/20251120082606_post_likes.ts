e9bb5099fb1d961a353e68bfa2e53072b13c89dc
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-11-20 10:32:57 +0200

    Added post likes entity

---
+++ seeds/20251120082606_post_likes.ts
@@ -0,0 +1,31 @@
+import { Knex } from 'knex';
+import { DB_USER_SEEDS } from './20251102202749_users';
+import { PostLikeEntity } from '../src/schemas/entities/postLikeEntitySchema';
+
+export async function seed(knex: Knex): Promise<void> {
+	await knex('post_likes').del();
+
+	const seed = DB_USER_SEEDS.find((u) => u.username === 'test123');
+	if (!seed) throw new Error('No user seed found!');
+
+	const user = await knex('users').where({ username: seed.username }).first();
+	if (!user) {
+		throw new Error('User not found for posts seeding');
+	}
+
+	const userPosts = await knex('posts').where({userId: user.id}).limit(3);
+	if (!userPosts.length) {
+		throw new Error("User doesn't have posts!");
+	}
+
+	let counter = 0;
+	for (const post of userPosts) {
+		counter++;
+		const like: Partial<PostLikeEntity> = {
+			postId: post.id,
+			userId: user.id,
+		};
+
+		await knex('post_likes').insert(like);
+	}
+}
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,13 +1,15 @@
 import { db } from '../../config/knexConfig';
 import { CommentRepository } from './comments/CommentRepository';
 import { PostRepository } from './posts/PostRepository';
+import { PostLikesRepository } from './posts/PostLikesRepository';
 import { UserProfileRepository } from './users/UserProfileRepository';
 import { UserRepository } from './users/UserRepository';
 
 export const usersRepository = new UserRepository(db);
 export const userProfilesRepository = new UserProfileRepository(db);
 export const postsRepository = new PostRepository(db);
+export const postLikesRepository = new PostLikesRepository(db);
 export const commentsRepository = new CommentRepository(db);
 
 // Export classes for testing
-export { UserRepository, UserProfileRepository, PostRepository, CommentRepository };
+export { UserRepository, UserProfileRepository, PostRepository, PostLikesRepository, CommentRepository };
