e6aa5a797d0c713757951101ba6136b00e311561
Azzarox <62171309+Azzarox@users.noreply.github.com>
2026-01-14 18:29:56 +0200

    Fixed path for prod

--- config/swaggerConfig.ts
+++ config/swaggerConfig.ts
@@ -2,5 +2,7 @@ import { koaSwagger } from "koa2-swagger-ui";
 import yamljs from "yamljs";
 import path from "path";
 
-const spec = yamljs.load(path.join(__dirname, '../swagger.yml'));
+const spec = yamljs.load(
+  path.resolve(process.cwd(), 'swagger.yml')
+);
 export default koaSwagger({routePrefix: false, swaggerOptions: { spec }});
