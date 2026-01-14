cfba237e35098a2c35430e690a80fc178ffe1109
Azzarox <62171309+Azzarox@users.noreply.github.com>
2026-01-14 13:11:54 +0200

    Added swagger

---
+++ config/swaggerConfig.ts
@@ -0,0 +1,6 @@
+import { koaSwagger } from "koa2-swagger-ui";
+import yamljs from "yamljs";
+import path from "path";
+
+const spec = yamljs.load(path.join(__dirname, '../swagger.yml'));
+export default koaSwagger({routePrefix: false, swaggerOptions: { spec }});
